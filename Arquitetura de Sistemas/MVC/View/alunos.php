fc72e755d7f1b145aa7260a89e25a9a055991396
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-08-06 11:54:06 -0300

    Commit - Victor - 06/08/2025

--- Arquitetura de Sistemas/MVC/View/alunos.php
+++ Arquitetura de Sistemas/MVC/View/alunos.php
@@ -5,8 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de Alunos</title>
 </head>
-<body>
+<>
     <h1>Alunos</h1>
-    
+
+    <ul>
+    <?php foreach ($alunos as $aluno) { ?>
+        <li> <?php echo "Nome: " . $aluno ['nome'] ." || Idade: " . $aluno ['idade'];?> </li>
+        <?php } ?>
+    </ul>
+
+    <h1>Adicionar Alunos</h1>
+    <form action="acao = adicionar">
+
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required placeholder="Coloque o nome do aluno"> <br>
+
+        <label for="idade">Idade:</label>
+        <input type="text" name="idade" id="idade" required placeholder="Informe a idade do aluno"> <br>
+
+        <button type = "submit">Adicionar</button>
+
+    </form>
+
 </body>
 </html>
