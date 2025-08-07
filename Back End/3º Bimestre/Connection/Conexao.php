17e7982350c745ea53891dc4c731daa3b2e57723
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-08-07 11:48:02 -0300

    Commit - Victor - 07/08/2025

---
+++ Back End/3º Bimestre/Connection/Conexao.php
@@ -0,0 +1,27 @@
+<?php
+
+    class Conexao {
+        private $dbName = "joao_lopes";
+        private $host = "localhost";
+        private $user = "root";
+        private $password = "1234";
+        protected ?PDO $conn = null;
+
+        public function connect(): ?PDO {
+            try {
+                $this->conn = new PDO("mysql: host=$this->host; dbname=$this->dbName", $this->user, $this->password);
+                return $this->conn;
+            }
+            catch(PDOException $e){
+                echo "Erro na conexão" . $e->getMessage();
+                return null;
+            }
+
+        }
+
+    }
+
+    $conexao = new Conexao;
+    $conexao->connect();
+
+?>
