92dbff1d777ef55026fa8fe720b625500cb664a5
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-08-12 11:51:29 -0300

    Commit - Victor - 12/08/2025

---
+++ Back End/3º Bimestre/CRUD/Aluno.php
@@ -0,0 +1,21 @@
+<?php
+    require_once "Conexao.php";
+
+    class Aluno {
+        private ?PDO $conn;
+
+        public function __construct(){
+            $db = new Conexao();
+            $this->conn = $db->connect();
+        }
+
+        public function create(string $name, string $email): bool {
+            $sql = "INSERT INTO alunos (nome, email) VALUES (:name, :email)";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":email", $email);
+            return $stmt->execute()
+        }
+    }
+
+?>
