4005245192165c4119281b768a020d65075bdfa4
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-09-03 08:37:36 -0300

    Commit - Larissa - 03/09/2025

---
+++ todo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.todo',
+  appName: 'todo',
+  webDir: 'www'
+};
+
+export default config;
