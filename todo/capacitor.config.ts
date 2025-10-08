1d3a26312f344c7903efda5a50740fe76a898c08
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-10-08 11:08:09 -0300

    Commit - Milagre emergente

---
+++ todo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.todo',
+  appName: 'Todo App',
+  webDir: 'www/browser'
+};
+
+export default config;
