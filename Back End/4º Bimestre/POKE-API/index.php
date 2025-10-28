67c996dd12311bffde80305f3663e46f8959d6a9
juliocesarfonseca003@gmail.com <juliocesarfonseca003@gmail.com>
2025-10-28 11:49:47 -0300

    `Added search functionality to Poke API index.php, including form and conditional display of Pokémon data.`

--- Back End/4º Bimestre/POKE-API/index.php
+++ Back End/4º Bimestre/POKE-API/index.php
@@ -18,6 +18,7 @@ if (isset($_GET['q']) && $_GET['q'] !== "") {
         echo "ERRO AO DECODIFICAR O JSON.";
         exit;
     }
+
 }
 
 ?>
@@ -28,13 +29,25 @@ if (isset($_GET['q']) && $_GET['q'] !== "") {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
+        <title>Poke API</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     </head>
 
     <body>
-        <h1>Hello, world!</h1>
-
+        
+        <div class="container text-center">
+            <div class="card shadow-lg p-4" style="max-width: 500px; margin: auto;">
+                <h1 class="mb-4">PokeDex</h1>
+                <form method="get" class="d-flex justify-content-center mb-4">
+                <input type="text" class="form-control w-50 h-50 me-2" name="q" id="q" required placeholder="Informe o nome do Pokémon">
+                <button class="btn btn-success">Procurar</button>
+                </form>
+                <?php if($data):?>
+                    <div class="card-body">
+                        <h2 class="card-title text-capitalize"><?php echo $data['name'];?></h2> <?php endif ?>
+                    </div>
+            </div>
+        </div>
         
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
